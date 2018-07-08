888e22ae596e9f5bd89069b65e7fbc158b36a18a
Victoria Batranak <vik1126111@gmail.com>
2018-07-08 13:15:31 +0300

    config chaneges

--- common/config/main.php
+++ common/config/main.php
@@ -1,5 +1,5 @@
 <?php
-return [
+/*return [
     'name' => 'Blog',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
@@ -11,4 +11,75 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
     ],
+];*/
+return [
+    'id' => 'app-frontend',
+    'name' => 'Blog',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\Module',
+        ],
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'vik1126111@gmail.com',
+                'password' => '',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+        'request' => [
+            'csrfParam' => '_csrf-frontend',
+        ],
+        'user' => [
+            'identityClass' => '\common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+            'loginUrl' => ['auth/login'],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-frontend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<action_site:(category|single|tag)>/<id:\d+>' => 'site/<action_site>',
+                '/about' => 'site/about',
+                '/contact' => 'site/contact',
+            ],
+        ],
+
+    ],
+    'params' => $params,
 ];
